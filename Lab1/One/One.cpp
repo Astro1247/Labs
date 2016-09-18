e00094ccb972407c166bddde80c2372d89e09953
Astro <taynik777@gmail.com>
2016-09-18 18:17:32 +0300

    V1.0

--- Lab1/One/One.cpp
+++ Lab1/One/One.cpp
@@ -6,6 +6,14 @@
 
 int main()
 {
-    return 0;
+	float v = 50, s = 10, h;
+	h = v / s;
+	printf("Result usng float (height): %f", h);
+	int v1 = 50, s1 = 10, h1;
+	h1 = v1 / s1;
+	printf("\nResult usng int   (height): %i", h1);
+	printf("\nPress ENTER to continue...");
+	getchar();
+	return 0;
 }
 
--- Lab1/Two/Two.cpp
+++ Lab1/Two/Two.cpp
@@ -6,6 +6,29 @@
 
 int main()
 {
-    return 0;
+	//using float
+	float x = 10, y = 15, first, second, chisl1, chisl2, znam1, znam2, result;
+	chisl1 = x + y;
+	znam1 = y + 1;
+	first = chisl1 / znam1;
+	chisl2 = (x*y) - 12;
+	znam2 = 34 + x;
+	second = chisl2 / znam2;
+	result = first - second;
+	printf("Result usint float: %f", result);
+
+	//using int
+	int x_ = 10, y_ = 15, first_, second_, chisl1_, chisl2_, znam1_, znam2_, result_;
+	chisl1_ = x_ + y_;
+	znam1_ = y_ + 1;
+	first_ = chisl1_ / znam1_;
+	chisl2_ = (x_*y_) - 12;
+	znam2_ = 34 + x_;
+	second_ = chisl2_ / znam2_;
+	result_ = first_ - second_;
+	printf("\nResult usint float: %i", result_);
+	printf("\nPress ENTER to continue...");
+	getchar();
+	return 0;
 }
 
