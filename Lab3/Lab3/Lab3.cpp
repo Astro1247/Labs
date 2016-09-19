84c78dfb4617267fe064829d73f54cbf71e475ad
Astro <taynik777@gmail.com>
2016-09-19 12:27:15 +0300

    Final_checked

--- Lab3/Lab3/Lab3.cpp
+++ Lab3/Lab3/Lab3.cpp
@@ -2,25 +2,44 @@
 //
 
 #include "stdafx.h"
+#include "stdlib.h"
+#include "stdio.h"
+#include "conio.h"
+#include "math.h"
+#include "locale.h"
+#include "string.h"
 
 
 int main()
 {
-	int input1, input2;
-	printf("Input number to check: ");
+	setlocale(LC_ALL, "RUS");
+	int input1, input2, input3;
+	func1:
+	printf("Введите число для проверки: ");
 	scanf("%d", &input1);
 	if (input1 < 0)
 	{
-		printf("That was negative number.\n");
+		printf("Это было отрицательное число.\n");
+	}
+	else if (input1 > 0)
+	{
+		printf("Это было положительное число.\n");
 	}
 	else
 	{
-		printf("That was positive nubmer.\n");
+		printf("Это был ноль.\n");
+	}
+	printf("Повторить проверку? (0 - нет/1 - да)");
+	scanf("%d", &input3);
+	if (input3 == 1)
+	{
+		goto func1;
 	}
 	getchar();
-	printf("Input number to change to equival symbol: ");
+	func2:
+	printf("Введите число для замены на еквивалентный символ: ");
 	scanf("%d", &input2);
-	printf("Equival symbol: ");
+	printf("Еквивалентный символ: ");
 	switch (input2)
 	{
 		case 1:
@@ -74,7 +93,12 @@ int main()
 			break;
 		}
 	}
-	getchar();
+	printf("\nПовторить проверку? (0 - нет/1 - да)");
+	scanf("%d", &input3);
+	if (input3 == 1)
+	{
+		goto func2;
+	}
 	getchar();
     return 0;
 }
