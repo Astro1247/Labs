5b8b8c53783ff1d54f0620de7127963eaa3ffa37
Astro <taynik777@gmail.com>
2016-09-19 13:44:05 +0300

    added some comment for test

--- Lab4/Lab4/Lab4.cpp
+++ Lab4/Lab4/Lab4.cpp
@@ -2,12 +2,19 @@
 //
 
 #include "stdafx.h"
-
-
-int main()
+#include "stdlib.h"
+#include "stdio.h"																	 
+#include "conio.h"																	 
+#include "math.h"																	 
+#include "locale.h"																	 
+#include "string.h"																	 
+																					 
+																					 
+int main()																			 
 {
-	int s = 0;
-	do
+	setlocale(LC_ALL, "RUS");
+	int s = 0;																		 
+	do																				 
 	{
 		printf("S<50, increasing S by 1... (S=%i)\n", s);
 		s = s + 1;
@@ -17,6 +24,7 @@ int main()
 	printf("Now S = %i\n", s);
 	printf("Press ENTER to continue...\n");
 	getchar();
+	//123
 	return 0;
 }
 
