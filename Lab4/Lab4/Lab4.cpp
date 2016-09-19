a3c72420b23471f02736d6f232973c7bd963bdcb
Astro <taynik777@gmail.com>
2016-09-19 11:56:00 +0300

    First part done

--- Lab4/Lab4/Lab4.cpp
+++ Lab4/Lab4/Lab4.cpp
@@ -6,6 +6,17 @@
 
 int main()
 {
-    return 0;
+	int s = 0;
+	do
+	{
+		printf("S<50, increasing S by 1... (S=%i)\n", s);
+		s = s + 1;
+		printf("Checking if S<50...\n");
+	} while (s < 50);
+	printf("Stopping loop..\n");
+	printf("Now S = %i\n", s);
+	printf("Press ENTER to continue...\n");
+	getchar();
+	return 0;
 }
 
