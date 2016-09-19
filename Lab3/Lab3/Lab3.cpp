13b75666da92f78ef8dbefbfe23c8a37895a9872
Astro <taynik777@gmail.com>
2016-09-19 11:45:39 +0300

    Part two and some fixes to first part

--- Lab3/Lab3/Lab3.cpp
+++ Lab3/Lab3/Lab3.cpp
@@ -11,13 +11,69 @@ int main()
 	scanf("%d", &input1);
 	if (input1 < 0)
 	{
-		printf("Negative number.\n");
+		printf("That was negative number.\n");
 	}
 	else
 	{
-		printf("Positive nubmer.\n");
+		printf("That was positive nubmer.\n");
 	}
-	printf("\nPress ENTER to continue...\n");
+	getchar();
+	printf("Input number to change to equival symbol: ");
+	scanf("%d", &input2);
+	printf("Equival symbol: ");
+	switch (input2)
+	{
+		case 1:
+		{
+			printf("!");
+			break;
+		}
+		case 2:
+		{
+			printf("@");
+			break;
+		}
+		case 3:
+		{
+			printf("#");
+			break;
+		}
+		case 4:
+		{
+			printf("$");
+			break;
+		}
+		case 5:
+		{
+			printf("%%");
+			break;
+		}
+		case 6:
+		{
+			printf("^");
+			break;
+		}
+		case 7:
+		{
+			printf("&");
+			break;
+		}
+		case 8:
+		{
+			printf("*");
+			break;
+		}
+		case 9:
+		{
+			printf("(");
+			break;
+		}
+		case 0:
+		{
+			printf(")");
+			break;
+		}
+	}
 	getchar();
 	getchar();
     return 0;
