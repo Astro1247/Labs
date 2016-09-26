b582b70bb8881d9bae773599f8c8f5204c17f0e1
Astro <taynik777@gmail.com>
2016-09-26 17:22:17 +0300

    Добавил 2 часть, ловит ошибку на выходе из цикла

--- Lab4/Lab4/Lab4.cpp
+++ Lab4/Lab4/Lab4.cpp
@@ -6,39 +6,53 @@
 int main()
 {
 	setlocale(LC_ALL, "RUS");
-func1:
+	int s, f, input[4], i, temp;
 	char yn;
-	int s = 0, f = 0;
 	do
 	{
-		f = f + 1;
-		//printf("S меньше 50, увеличиваем S на 1... (S=%i)\n", s);
-		s = s + f;
-		printf("Проверка на выполнение условия S<50...\nS= %i\n", s);
-	} while (s < 50);
-	printf("Остановка цикла..\n");
-	printf("Сейчас S равен %i\n", s);
-check1:
-	printf("Повторить? (y/n)\n");
-	scanf(" %c", &yn);
-	if (yn == 'n' || yn == 'N')
-	{
-		goto func2;
-	}
-	else if (yn == 'Y' || yn == 'y')
-	{
-		goto func1;
-		printf("\n");
-	}
-	else
-	{
-		printf("Некорректный ввод.\n");
-		goto check1;
-	}
-func2:
-
+		s = 0;
+		f = 0;
+		do
+		{
+			f = f + 1;
+			//printf("S меньше 50, увеличиваем S на 1... (S=%i)\n", s);
+			s = s + f;
+			printf("Проверка на выполнение условия S<50...\nS= %i\n", s);
+		} while (s < 50);
+		printf("Остановка цикла..\n");
+		printf("Сейчас S равен %i\n", s);
+		/*printf("Повторить? (y/n)\n");
+		scanf(" %c", &yn);
+		if (yn == 'n' || yn == 'N')
+		{
+			goto func2;
+		}
+		else if (yn == 'Y' || yn == 'y')
+		{
+			goto func1;
+			printf("\n");
+		}
+		else
+		{
+			printf("Некорректный ввод.\n");
+			goto check1;
+		}*/
+		printf("Повторить? (Y/N)");
+		scanf(" %c", &yn);
+	}  while (yn == 'y' && yn != 'n');
 	printf("Нажмите ENTER чтобы продолжить . . .\n");
 	getchar();
+	do
+	{
+		for (i = 0; i < 5; i++)
+		{
+			printf("Введите число %i: ", i);
+
+			scanf("%d", &input[i]);
+		}
+		printf("Повторить? (Y/N)");
+		scanf(" %c", &yn);
+	} while (yn == 'y' && yn != 'n');
 	return 0;
 }
 
