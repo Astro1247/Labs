2080c01ec0a499e51a50a3fba1627635affee181
Astro <taynik777@gmail.com>
2016-09-28 16:54:40 +0300

    Deleted comments

--- Lab4/Lab4/Lab4.cpp
+++ Lab4/Lab4/Lab4.cpp
@@ -15,28 +15,11 @@ int main()
 		do
 		{
 			f = f + 1;
-			//printf("S меньше 50, увеличиваем S на 1... (S=%i)\n", s);
 			s = s + f;
 			printf("Проверка на выполнение условия S<50...\nS= %i\n", s);
 		} while (s < 50);
 		printf("Остановка цикла..\n");
 		printf("Сейчас S равен %i\n", s);
-		/*printf("Повторить? (y/n)\n");
-		scanf(" %c", &yn);
-		if (yn == 'n' || yn == 'N')
-		{
-			goto func2;
-		}
-		else if (yn == 'Y' || yn == 'y')
-		{
-			goto func1;
-			printf("\n");
-		}
-		else
-		{
-			printf("Некорректный ввод.\n");
-			goto check1;
-		}*/
 		printf("Повторить? (Y/N)");
 		scanf(" %c", &yn);
 	}  while (yn == 'y' && yn != 'n');
