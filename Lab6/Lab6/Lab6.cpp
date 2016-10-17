a4a12ac8daebe92a9e0d42e5a433d7396af5ec9d
Astro <taynik777@gmail.com>
2016-10-17 17:54:44 +0300

    removed not working

--- Lab6/Lab6/Lab6.cpp
+++ Lab6/Lab6/Lab6.cpp
@@ -2,26 +2,14 @@
 #include <locale.h>
 #include "stdlib.h"
 
-void remove_all_chars(char* string, char c) {
-	char *pr = string, *pw = string;
-	while (*pr) {
-		*pw = *pr++;
-		pw += (*pw != c);
-	}
-	*pw = '\0';
-}
-
 int main()
 {
 	char *invalidch = "o";
-	char string[100], delch;
+	char string[100];
 	setlocale(LC_ALL, "RUS");
 	printf("¬ведите строку: ");
 	scanf("%s", string);
 	char *c = string;
-	scanf("%c", &delch);
-	remove_all_chars(string, 'o');
-	printf("%s\n", string);
 	system("pause");
     return 0;
 }
