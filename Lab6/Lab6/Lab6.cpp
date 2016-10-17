ee2a53342514e0411111ae7568a590ea6a9370ef
Astro <taynik777@gmail.com>
2016-10-17 13:46:38 +0300

    added working first part

--- Lab6/Lab6/Lab6.cpp
+++ Lab6/Lab6/Lab6.cpp
@@ -1,11 +1,27 @@
-// Lab6.cpp: определ€ет точку входа дл€ консольного приложени€.
-//
-
 #include "stdafx.h"
+#include <locale.h>
+#include "stdlib.h"
 
+void remove_all_chars(char* string, char c) {
+	char *pr = string, *pw = string;
+	while (*pr) {
+		*pw = *pr++;
+		pw += (*pw != c);
+	}
+	*pw = '\0';
+}
 
 int main()
 {
+	char *invalidch = "o";
+	char string[100], delch;
+	setlocale(LC_ALL, "RUS");
+	printf("¬ведите строку: ");
+	scanf("%s", string);
+	char *c = string;
+	scanf("%c", &delch);
+	remove_all_chars(string, 'o');
+	printf("%s\n", string);
+	system("pause");
     return 0;
 }
-
