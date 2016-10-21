f848d281b715261cb412dd50970e976475fba413
Astro <taynik777@gmail.com>
2016-10-22 01:09:10 +0300

    upd

--- Lab1/Lab1/Lab1.cpp
+++ Lab1/Lab1/Lab1.cpp
@@ -1,6 +1,3 @@
-// Lab1.cpp: определяет точку входа для консольного приложения.
-//
-
 #include "stdafx.h"
 #include "locale.h"
 #include <stdlib.h>
@@ -13,9 +10,9 @@ void Swap(int *Mas, int i, int N2, int K2)
 	temp = Mas[i];
 	Mas[i] = Mas[i - 1];
 	Mas[i - 1] = temp;
-	printf("\nСостояние массива: ");
-	for (K2 = 0; K2<N2; K2++)
-		printf("%i, ", Mas[K2]);
+	//printf("\nСостояние массива: ");
+	//for (K2 = 0; K2<N2; K2++)
+		//printf("%i, ", Mas[K2]);
 }
 
 void ShakerSort(int *Mas, int Start, int N, int K)
@@ -53,12 +50,12 @@ void main()
 		scanf("%d", &A[k]);
 		getchar();*/
 		A[k] = rand() % (500 + 1 - 0) + 0;
-		printf("%i элемент: %i\n", temp1, A[k]);
+		//printf("%i элемент: %i\n", temp1, A[k]);
 	}
 	ShakerSort(A, 1, n, k);
 	printf("\nРезультирующий массив: ");
-	for (k = 0; k<n; k++) 
-		printf("%i, ", A[k]);
+	/*for (k = 0; k<n; k++) 
+		printf("%i, ", A[k]);*/
 	clock_t end = clock();
 	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
 	printf("\nПотраченное время: %f секунд\n", time_spent);
