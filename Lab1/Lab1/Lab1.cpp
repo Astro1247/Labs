2aea965ae6bb63daaa3d935e70f90c7da1ee1300
Astro <taynik777@gmail.com>
2016-10-22 10:48:42 +0300

    some fix

--- Lab1/Lab1/Lab1.cpp
+++ Lab1/Lab1/Lab1.cpp
@@ -4,18 +4,18 @@
 #include <time.h>
 
 
-void Swap(int *Mas, int i, int N2, int K2)
+int Swap(int *Mas, int i, int N2, int K2)
 {
 	int temp;
 	temp = Mas[i];
 	Mas[i] = Mas[i - 1];
 	Mas[i - 1] = temp;
-	//printf("\nСостояние массива: ");
-	//for (K2 = 0; K2<N2; K2++)
-		//printf("%i, ", Mas[K2]);
+	printf("\nСостояние массива: ");
+	for (K2 = 0; K2<N2; K2++)
+		printf("%i, ", Mas[K2]);
 }
 
-void ShakerSort(int *Mas, int Start, int N, int K)
+int ShakerSort(int *Mas, int Start, int N, int K)
 {
 	int Left, Right, i, N1 = N, K1 = K;
 	Left = Start;
@@ -35,32 +35,33 @@ void ShakerSort(int *Mas, int Start, int N, int K)
 	}
 }
 
-void main()
+int main()
 {
-	int n, k, temp1;
+	int n = 10, k, temp1, A[10];
 	setlocale(LC_ALL, "RUS");
-	printf("Размер массива: ");
-	scanf("%d", &n);
+	/*printf("Размер массива: ");
+	scanf("%d", &n);*/
 	clock_t begin = clock();
-	int *A = new int[n];
+	//int *A = new int[n];
+	
 	for (k = 0; k<n; k++)
 	{
 		temp1 = k + 1;
-		/*printf("%i элемент: ", tt1);
+		/*printf("%i элемент: ", temp1);
 		scanf("%d", &A[k]);
 		getchar();*/
 		A[k] = rand() % (500 + 1 - 0) + 0;
-		//printf("%i элемент: %i\n", temp1, A[k]);
+		printf("%i элемент: %i\n", temp1, A[k]);
 	}
 	ShakerSort(A, 1, n, k);
 	printf("\nРезультирующий массив: ");
-	/*for (k = 0; k<n; k++) 
-		printf("%i, ", A[k]);*/
+	for (k = 0; k<n; k++) 
+		printf("%i, ", A[k]);
 	clock_t end = clock();
 	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
 	printf("\nПотраченное время: %f секунд\n", time_spent);
 	printf("Нажмите ENTER для продолжения  . . . ");
 	getchar();
-	getchar();
+	return 0;
 }
 
